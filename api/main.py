b2ad8c0200790dd9bca4d402944dc6a79ebc47d6
TAEHYUN SUNG <SUNGTA@tdm.local>
2024-04-23 15:00:46 +0800

    ready for submission

--- api/main.py
+++ api/main.py
@@ -19,3 +19,11 @@ api.add_middleware(AuthenticationMiddleware, backend=backend)
 @api.get('/')
 async def index():
     return 'Hello, world.'
+
+@api.get('/ds1/res1')
+async def ds1_res1():
+    return "[Accessed] ds1/res1"
+
+@api.get('/ds2/res2')
+async def ds2_res2():
+    return "[Accessed] ds2/res2"
