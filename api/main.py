e8110043aed2b07b8054b67664ddb853d22e4fd1
TAEHYUN SUNG <SUNGTA@tdm.local>
2024-04-23 14:44:02 +0800

    continue on question 7 on word document

--- api/main.py
+++ api/main.py
@@ -1,5 +1,9 @@
 import casbin
 from fastapi import FastAPI
+from fastapi_authz import CasbinMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+
+import basic_auth
 
 api = FastAPI()
 
@@ -7,6 +11,10 @@ enforcer = casbin.Enforcer(
     'rbac_model.conf',
     'rbac_policy.csv',
 )
+backend = basic_auth.BasicAuth()
+
+api.add_middleware(CasbinMiddleware, enforcer=enforcer)
+api.add_middleware(AuthenticationMiddleware, backend=backend)
 
 @api.get('/')
 async def index():
